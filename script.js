1281c3afee874291c6960c933a83cfe2cc9a478c
nikhar pandya <pandyanikhar@gmail.com>
2021-08-11 15:34:04 -0400

    dynamic background and layout changes

--- script.js
+++ script.js
@@ -18,20 +18,20 @@ function getResults(query) {
 }
 
 function displayResults(weather) {
-  let city = document.querySelector(".location .city");
+  let city = document.querySelector(".city");
   city.innerText = `${weather.name}, ${weather.sys.country}`;
 
   let now = new Date();
-  let date = document.querySelector(".location .date");
+  let date = document.querySelector(".date");
   date.innerText = dateBuilder(now);
 
-  let temp = document.querySelector(".current .temp");
+  let temp = document.querySelector(".temp");
   temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
-  let feel = document.querySelector(".current .feels-like");
+  let feel = document.querySelector(".feels-like");
   feel.innerHTML = `${Math.round(weather.main.feels_like)}<span>°c</span>`;
 
-  let weather_el = document.querySelector(".current .weather");
+  let weather_el = document.querySelector(".weather");
   weather_el.innerText = weather.weather[0].main;
 
   let hilow = document.querySelector(".hi-low");
@@ -44,6 +44,35 @@ function displayResults(weather) {
 
   let sunset = document.querySelector(".sun .sunset");
   sunset.innerText = `${format_time(weather.sys.sunset)}`;
+
+  let weather_icon = document.querySelector(".icon");
+  let idIcon = weather.weather[0].icon;
+  weather_icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${idIcon}@2x.png" alt="icon" />`;
+
+  dynamicBG(weather.weather[0].id);
+}
+
+function dynamicBG(i) {
+  const bg = document.querySelector("#background");
+
+  if ((i >= 200) & (i < 300)) {
+    bg.classList.remove("atmosphere");
+    bg.classList.add("rain");
+  } else if ((i >= 300) & (i < 600)) {
+    bg.classList.remove("atmosphere");
+    bg.classList.add("rain");
+  } else if ((i >= 600) & (i < 700)) {
+    bg.classList.remove("atmosphere");
+    bg.classList.add("snow");
+  } else if ((i >= 701) & (i < 800)) {
+    bg.className = "atmosphere";
+  } else if (i === 800) {
+    bg.classList.remove("atmosphere");
+    bg.classList.add("clear");
+  } else if (i > 801) {
+    bg.classList.remove("atmosphere");
+    bg.classList.add("cloud");
+  }
 }
 
 function format_time(s) {
